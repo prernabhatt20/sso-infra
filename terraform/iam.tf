58776f58b794ecb367f8a4e43fdd1fd5fe495d17
Prerna Bhatt <prerna.bhatt@rtctek.global>
2026-02-02 15:05:50 +0530

    change iam name

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
+  name = "ecsTaskExecutionRoleNew"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
