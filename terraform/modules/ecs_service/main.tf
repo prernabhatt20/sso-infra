3c5bdfe51aa83a18b845b9e72c0c333d2736faf5
Prerna Bhatt <prerna.bhatt@rtctek.global>
2026-02-03 11:06:33 +0530

    adding rolling deployment

--- terraform/modules/ecs_service/main.tf
+++ terraform/modules/ecs_service/main.tf
@@ -25,6 +25,11 @@ resource "aws_ecs_service" "ecs-service" {
   desired_count   = 1
   launch_type     = "FARGATE"
 
+  # Rolling deployment
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+  force_new_deployment               = true
+
   network_configuration {
     subnets          = var.subnet_ids
     security_groups  = [aws_security_group.sg.id]
