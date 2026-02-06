81e919d6eea3a46c04d01a1d5e62257e7fd74af1
narendraacs <n_kumar@acs.org>
2026-02-06 16:47:48 +0530

    add oidc

---
+++ terraform/github-actions-role.tf
@@ -0,0 +1,32 @@
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-oidc-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:prernabhatt20/sso-infra/*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+
+# Attach policy to GitHub role
+resource "aws_iam_role_policy_attachment" "github_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
